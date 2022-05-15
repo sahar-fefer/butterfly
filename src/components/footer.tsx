679104e1378474576a555c2b4e5f219350dc9d3f
sahar <sahar.fefer@gmail.com>
2022-05-15 08:59:49 +0300

    update gatsby-config.ts

---
+++ src/components/footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from "gatsby"
+import "../styles/nav.scss"
+import { DonateHart } from "../svg"
+
+interface Props { }
+
+function Footer(props: Props) {
+    const { } = props
+
+    return (
+        <footer>
+
+        </footer>
+    )
+}
+
+export default Footer
