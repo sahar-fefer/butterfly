d5e458f960a90526f0235cf5053b2855fa356c9f
sahar <sahar.fefer@gmail.com>
2022-09-05 16:24:54 +0300

    update 5 files and delete 1 file

--- src/components/index/mainBanner.tsx
+++ src/components/index/mainBanner.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { DonateHart } from "../../svg"
-import BannerImg from '../../images/main-banner.jpg'
+import MobileBannerImg from '../../images/main-banner-m.jpg'
+import FullBannerImg from '../../images/main-banner-d.jpg'
 import "../../styles/main-banner.scss"
 import { Link } from 'gatsby'
 
@@ -11,14 +12,18 @@ function MainBanner(props: Props) {
 
     return (
         <section className='main-banner'>
-            <img className='banner' src={BannerImg} alt="" />
+            <picture>
+                <source media="(min-width: 768px)" srcSet={FullBannerImg} />
+                <img className='banner' src={MobileBannerImg} alt="" />
+            </picture>
+            {/* <img className='banner' src={BannerImg} alt="" /> */}
             <div className="inner-text">
                 <h1>עומר השארת לנו מתנה,<br />
                     אנחנו מעבירות אותה הלאה</h1>
-                <div className="sub-title">עזרת לנו לראות, אנחנו נמשיך להפיץ את אורך, טוב ליבך ושמחת החיים שלך.</div>
+                <div className="sub-title">עזרת לנו לראות, אנחנו נמשיך להפיץ את אורך, <br />טוב ליבך ושמחת החיים שלך.</div>
                 <div className="ctas">
+                    <Link to="/" className="donate"><DonateHart color='black' />לתרומה</Link>
                     <Link to="/" className="volunteer">להתנדבות</Link>
-                    <Link to="/" className="donate">לתרומה <DonateHart color='black' /></Link>
                 </div>
             </div>
         </section>
