4cd9723239aa9b6fd9dc9587a3a66263c5ce7ed0
sahar <sahar.fefer@gmail.com>
2022-09-25 10:01:08 +0300

    Updates

---
+++ component---src-pages-events-tsx-70fc25800929613e66b9.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbutterfly=self.webpackChunkbutterfly||[]).push([[512],{8279:function(e,t,l){l.r(t);var n=l(7294),a=l(4249);t.default=function(){return n.createElement(a.Z,null,n.createElement("div",{className:"events fill-page"},n.createElement("h1",null,"אירועים"),n.createElement("div",{className:"sub-title"},"העמוד יעלה בקרוב...")))}}}]);
+//# sourceMappingURL=component---src-pages-events-tsx-70fc25800929613e66b9.js.map
