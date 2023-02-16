1d51e97d6914848e1faab3db5d8026d539bf4361
sahar <sahar.fefer@gmail.com>
2023-02-16 16:29:23 +0200

    Updates

--- component---src-pages-omers-butterflies-tsx-77a1aa93f5b90314637e.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkbutterfly=self.webpackChunkbutterfly||[]).push([[276],{8899:function(e,t,l){l.r(t);var u=l(7294),a=l(4249);t.default=function(){return u.createElement(a.Z,null,u.createElement("div",{className:"omers-butterflies fill-page"},u.createElement("h1",null,"הפרפרים של עומר"),u.createElement("div",{className:"sub-title"},"העמוד יעלה בקרוב...")))}}}]);
-//# sourceMappingURL=component---src-pages-omers-butterflies-tsx-77a1aa93f5b90314637e.js.map
--- component---src-pages-omers-house-tsx-29c62fbe0f1c49bda1fa.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkbutterfly=self.webpackChunkbutterfly||[]).push([[997],{388:function(e,t,l){l.r(t);var u=l(7294),a=l(4249);t.default=function(){return u.createElement(a.Z,null,u.createElement("div",{className:"omers-house fill-page"},u.createElement("h1",null,"הבית של עומר"),u.createElement("div",{className:"sub-title"},"העמוד יעלה בקרוב...")))}}}]);
-//# sourceMappingURL=component---src-pages-omers-house-tsx-29c62fbe0f1c49bda1fa.js.map
